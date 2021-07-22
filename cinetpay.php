7b2d3d11f365db99c0dab17f2a9e1ada4972f409
slydesin <77005293+slydesign1@users.noreply.github.com>
2021-07-22 08:25:53 +0100

    last update

--- cinetpay.php
+++ cinetpay.php
@@ -16,11 +16,11 @@ $signatureUrl = "https://api.cinetpay.com/v1/?method=getSignatureByPost";
 $cpm_amount = 100; //TransactionAmount
 $cpm_custom = '';// This field exist soanything can be inserted in it;it will be send back after payment
 
-$cpm_designation = 'VavavoomPayement'; //this field exist to identify the article being paid
+$cpm_designation = 'testcinetpay'; //this field exist to identify the article being paid
 
 
 $cpm_trans_date = date("Y-m-d H:i:s");
-$cpm_trans_id = 'Vavavoom-' . (string)date("YmdHis"); //Transaction id that will be send to identify the transaction
+$cpm_trans_id = 'testcinetpay-' . (string)date("YmdHis"); //Transaction id that will be send to identify the transaction
 $return_url = ""; //The customer will be redirect on this page after successful payment
 $cancel_url = "";//The customer will be redirect on this page if the payment get cancel
 $notify_url = "";//This page must be a webhook (webservice).
